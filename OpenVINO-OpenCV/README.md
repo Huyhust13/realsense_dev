710b9c515c162214b1f796e4e868e7a625ce5fcd
huyhust13 <huyhust13@gmail.com>
2019-06-14 17:38:50 +0700

    140619 chua fix duoc sai boudingbox o kich thuoc anh goc

--- OpenVINO-OpenCV/README.md
+++ OpenVINO-OpenCV/README.md
@@ -22,15 +22,25 @@ make
 
 `python image-classification.py`
 
-Tham khảo model tại: https://github.com/HoldenCaulfieldRye/caffe
+## Tham khảo model tại:
+https://github.com/HoldenCaulfieldRye/caffe
 
 Download model tại: https://github.com/HoldenCaulfieldRye/caffe/tree/master/models/bvlc_reference_rcnn_ilsvrc13 
 
-
-Than khảo:
-[1] https://davidmatablog.wordpress.com/2017/12/05/real-time-object-recognition-with-opencv-python-deep-learning-caffe-model/
+More SSD caffe model: https://github.com/weiliu89/caffe/tree/ssd
 
 
-update 130619
-* Vẽ boudingbox đang bị lệch
+
+## Tham khảo:
+[1] https://davidmatablog.wordpress.com/2017/12/05/real-time-object-recognition-with-opencv-python-deep-learning-caffe-model/  
+[2] https://www.pyimagesearch.com/2017/09/11/object-detection-with-deep-learning-and-opencv/
+
+## Các vấn đề:
+1. `blob = blobFromImage(frame, 0.007843, Size(300, 300));`  
+- frame là ảnh đầu vào, không yêu cầu kích thước
+- 0.007843 là tỉ số scale, không hiểu sao khi để là 1 thì k nhận dạng được, để 0.007843 thì nhận dạng được --> **Ý nghĩa và ảnh hưởng của thông số này là gì??**
+- Size(300,300) là kích thước ảnh đầu vào của mạng
+
+## Update 130619
+* Vẽ boudingbox đang bị lệch với kích thước frame thật
 * Cần bổ sung thêm về các chế độ input
--- OpenVINO-OpenCV/image-classification.cpp
+++ OpenVINO-OpenCV/image-classification.cpp
@@ -3,18 +3,21 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <string>
 
 using namespace std;
 using namespace cv;
 using namespace cv::dnn;
 
-static void show_usage(std::string name){
+static void show_usage(std::string name)
+{
     std::cerr << "Usage: " << name << " <option(s)> SOURCES\n"
               << "Option: \n"
               << "\t-h, --help\t\tShow this help message\n"
               << "\t-i, --image\t\tPath to image\n"
+              << "\t-w, --webcam\t\tdevice|default /dev/video0\n"
               << "default: use camera in /dev/video0"
-              <<endl;
+              << endl;
 }
 
 void writeMatToFile(cv::Mat &m, const char *filename)
@@ -38,17 +41,19 @@ void writeMatToFile(cv::Mat &m, const char *filename)
     fout.close();
 }
 
-int main(int argc, char* argv[])
+Mat detection(Mat frame, Net net, vector<string> classes );
+
+int main(int argc, char *argv[])
 {
     // debug = 0 in debug mode;
     bool debug = 0;
-    #pragma region load
+#pragma region load
     // load file model
     string root = "/media/huynv/Data/14.ComputerVision/1.IntelRealsense/realsense_dev/OpenVINO-OpenCV/";
     string labels_file = root + "caffemodel2/MobileNetSSD/labels.txt";
     string model = root + "caffemodel2/MobileNetSSD/MobileNetSSD_deploy.caffemodel";
-    string prototxt = root + "caffemodel2/MobileNetSSD/MobileNetSSD_deploy.prototxt";   
-    // load names of classes 
+    string prototxt = root + "caffemodel2/MobileNetSSD/MobileNetSSD_deploy.prototxt";
+    // load names of classes
     std::vector<string> classes;
     std::ifstream ifs(labels_file.c_str());
     if (!ifs.is_open())
@@ -69,86 +74,123 @@ int main(int argc, char* argv[])
     // net.setPreferableBackend(DNN_BACKEND_INFERENCE_ENGINE);
     net.setPreferableTarget(DNN_TARGET_CPU);
 
-    string str, outputFile;
-    VideoCapture cap;
-    VideoWriter video;
-    Mat frame, image, blob;
 
-    #pragma endregion load
-    
+#pragma endregion load
+    VideoCapture cap;
+    string str, device = "/dev/video0";
+    Mat frame;
+    int mode = 0;
     for (int i = 0; i < argc; i++)
-    {std::string arg = argv[i];
-        if ((arg == "-h") || (arg == "--help")) {
+    {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help"))
+        {
             show_usage(argv[0]);
             return 0;
         }
         if ((arg == "-i") || (arg == "--image"))
         {
-            str = argv[i+1];
+            str = argv[i + 1];
             ifstream ifile(str);
-            if (!ifile) throw("error");
-            cap.open(str);
-            // cout<<str<<endl;
-            // return 0; 
+            if (!ifile)
+                throw("error");
+            mode = 1;
+            break;
         }
-        else cap.open("/dev/video0");
-
+        else if((arg == "-w") || (arg == "--webcam"))
+        {
+            if (argc > i+1)
+            {
+                device = argv[i+1];
+            }
+            cap.open(device);
+            if (!cap.isOpened())
+            {
+                cout<<"Cannot open video from "<<device<<endl;
+                return 0;
+            }
+            mode = 2;
+            break;
+        }
     }
 
-    while (waitKey(1) < 0)
+    switch (mode)
     {
-        // get frame from video
-        cap >> frame;
-        // Mat image = imread(root + "images/cat.jpg", IMREAD_COLOR);
-        // cv::resize(frame, image, Size(300, 300));
-        blob = blobFromImage(frame, 0.007843, Size(300, 300));
-
-        // set the blob as input to the network and perform a forward-pass to
-        // obtain our output classification
-        net.setInput(blob);
-        Mat detection = net.forward();
-
-        double confidenceThreshold = 0.6;
-        cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
-
-        if (debug)
-        {
-            // ghi ra file txt
-            // Declare what you need
-            cv::FileStorage file("detectionMat.xml", cv::FileStorage::WRITE);
-            file << "detectionMatrix" << detectionMat;
-            writeMatToFile(detectionMat, "detectionMat.txt");
-        }
-
-        int frameWidth = frame.size[0];
-        int frameHeight = frame.size[1];
-
-        for (int i = 0; i < detectionMat.rows; i++)
-        {
-            float confidence = detectionMat.at<float>(i, 2);
-            if (confidence > confidenceThreshold)
-            {
-                int class_id = int(detectionMat.at<float>(i, 1));
-                string label = classes[class_id] + ": ";
-                label += to_string(confidence*100);
-                cout << label<< endl;
-
-                int x1 = static_cast<int>(detectionMat.at<float>(i, 3) *  frameWidth);
-                int y1 = static_cast<int>(detectionMat.at<float>(i, 4) *  frameHeight);
-                int x2 = static_cast<int>(detectionMat.at<float>(i, 5) *  frameWidth);
-                int y2 = static_cast<int>(detectionMat.at<float>(i, 6) * frameHeight);
-
-                cv::rectangle(frame, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 2, 4);
-                cv::putText(frame, label, Point(x1, y1), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0,0,255), 1);
-            }
-        }
-
-        double freq = getTickFrequency() / 1000;
-        std::vector<double> layersTimes;
-        double t = net.getPerfProfile(layersTimes) / freq;
-        cout << "[INFO] classification took " << t << " ms" << endl;
-
+    case 1: // detection in image
+        frame = imread(str, IMREAD_COLOR);
+        frame = detection(frame, net, classes);   
+        namedWindow("frame", CV_WINDOW_NORMAL);
         imshow("frame", frame);
+        waitKey(0);
+        return 0;
+    case 2: // detection in webcam
+        while (waitKey(1) < 0)
+        {
+            // get frame from video
+            cap >> frame;
+            frame = detection(frame, net, classes);   
+            // namedWindow("frame", CV_WINDOW_NORMAL);
+            imshow("frame", frame);
+            // imshow("image", image);
+        }
+    default:
+        break;
     }
     return 0;
+
+}
+
+Mat detection(Mat frame, Net net, vector<string> classes )
+{
+    Mat frame_resized, blob;
+    cv::resize(frame, frame, Size(300, 300),0,0,CV_INTER_LINEAR);
+    cv::resize(frame, frame_resized, Size(300, 300), 0, 0, CV_INTER_LINEAR);
+    // frame_resized = frame;
+    blob = blobFromImage(frame_resized, 0.007843, Size(300, 300), (127.5, 127.5, 127.5));
+
+    // set the blob as input to the network and perform a forward-pass to
+    // obtain our output classification
+    net.setInput(blob);
+    Mat detection = net.forward();
+
+    double confidenceThreshold = 0.6;
+    cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+
+    // ghi ra file txt
+    // Declare what you need
+    cv::FileStorage file("detectionMat.xml", cv::FileStorage::WRITE);
+    file << "detectionMatrix" << detectionMat;
+    writeMatToFile(detectionMat, "detectionMat.txt");
+
+
+    // size of frame resized
+    int frame_Width = frame.size[0];
+    int frame_Height = frame.size[1];
+    cout << "frame_size: " << frame_Width << ":" << frame_Height << endl;
+    for (int i = 0; i < detectionMat.rows; i++)
+    {
+        float confidence = detectionMat.at<float>(i, 2);
+        if (confidence > confidenceThreshold)
+        {
+            int class_id = int(detectionMat.at<float>(i, 1));
+            string label = classes[class_id] + ": " + to_string(confidence * 100) + "%";
+            cout << label << endl;
+
+            int xLeftTop = int(detectionMat.at<float>(i, 3) * frame_Width);
+            int yLeftTop = int(detectionMat.at<float>(i, 4) * frame_Height);
+            int xRightBottom = int(detectionMat.at<float>(i, 5) * frame_Width);
+            int yRightBottom = int(detectionMat.at<float>(i, 6) * frame_Height);
+            cv::rectangle(frame, cv::Point(xLeftTop, yLeftTop), cv::Point(xRightBottom, yRightBottom), cv::Scalar(0, 255, 0), 2, 4);
+            // cv::circle(frame, cv::Point(xLeftBottom,yLeftBottom), 2, Scalar(255,255,255), 3);
+            // cv::circle(frame, cv::Point(xRightTop,yRightTop), 2, Scalar(255,255,255), 3);
+            cv::putText(frame, label, Point(xLeftTop, yLeftTop), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
+            cout << detectionMat.at<float>(i, 3) << ":" << detectionMat.at<float>(i, 4) << endl;
+        }
+    }
+
+    double freq = getTickFrequency() / 1000;
+    std::vector<double> layersTimes;
+    double t = net.getPerfProfile(layersTimes) / freq;
+    cout << "[INFO] classification took " << t << " ms" << endl;
+    return frame;
 }
